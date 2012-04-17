6fccdbf1ed65a3afeff35ab9a13cb63c9e030567
sudevs <sudev.mukherjee@gmail.com>
2012-04-17 10:20:40 -0700

    Adding win_by vertical and win_horizontal that we did after coming back from dojo event. May interest others.

--- tictactoe.py
+++ tictactoe.py
@@ -7,9 +7,15 @@ def print_board():
 
 def win_by_diagonal(mark):
     return (board[0] == mark and board[4] == mark and board[8] == mark) or (board[6] == mark and board[4] == mark and board[2] == mark)
+    
+def win_by_vertical(mark):
+    return (board[0] == mark and board[3] == mark and board[6] == mark) or (board[1] == mark and board[4] == mark and board[7] == mark) or (board[2] == mark and board[5] == mark and board[8]== mark)
+
+def win_by_horizontal(mark):
+	return (board[0] == mark and board[1] == mark and board[2] == mark) or (board[3] == mark and board[4] == mark and board[5] == mark) or (board[6] == mark and board[7] == mark and board[8] == mark)
 
 def make_mark(mark, space):
     board[space] = mark
     print_board()
-    if win_by_diagonal(mark):
+    if win_by_diagonal(mark) or win_by_vertical(mark) or win_by_horizontal(mark):
         print mark + ' Wins!'
