1af03148815cfccb90d6e125ad52f65aebb16c5c
Kevin Clark <kevin.clark@gmail.com>
2012-05-12 14:15:06 -0700

    Flush out tic tac toe game loop

---
+++ tic-tac-toe-2/tic-tac-toe-2.md
@@ -0,0 +1,41 @@
+# Teaching the computer to play tic tac toe
+
+Today, we're going to take an existing tic tac toe game and teach the
+computer to play with us.
+
+
+
+### Installation
+
+#### Windows
+On windows, download and run the python 2.7 installer from
+http://www.python.org/ftp/python/2.7.3/python-2.7.3.msi
+
+After running the installer:
+
+1. Choose 'Install for all users', and click Next.
+2. Leave the default directory selected 'C:\Python27\', and click Next.
+3. Leave all options selected, and click Next.
+4. Click Yes if you get a User Account Control prompt asking for
+   permission to install software.
+5. Click Finish
+6. Click Yes to restart your computer.
+
+
+#### Mac OSX
+
+Mac OSX has python built in.
+
+
+
+### Starting the interpreter and basics
+
+On mac, you're going to open the python interpreter by hitting
+command-space, typing terminal, and hitting enter. Then you're going to
+type 'idle' and hit enter.
+
+On windows, you're going to open the python interpreter by hitting the
+windows key, typeing 'idle', and hit enter.
+
+
+
---
+++ tic-tac-toe-2/tictactoe.py
@@ -0,0 +1,58 @@
+board = [None, None, None, None, None, None, None, None, None]
+
+def print_board():
+    print board[0:3]
+    print board[3:6]
+    print board[6:9]
+
+def win_by_diagonal(mark):
+    return (board[0] == mark and board[4] == mark and board[8] == mark) or (board[6] == mark and board[4] == mark and board[2] == mark)
+
+def win_by_vertical(mark):
+    return (board[0] == mark and board[3] == mark and board[6] == mark) or (board[1] == mark and board[4] == mark and board[7] == mark) or (board[2] == mark and board[5] == mark and board[8]== mark)
+
+def win_by_horizontal(mark):
+	return (board[0] == mark and board[1] == mark and board[2] == mark) or (board[3] == mark and board[4] == mark and board[5] == mark) or (board[6] == mark and board[7] == mark and board[8] == mark)
+
+def validate_move(space):
+    if space < 0 or space > 8:
+        print "I don't understand how to make a mark at space %s" % space
+        return False
+    if board[space] is not None:
+        print "%s has already gone there!" % board[space]
+        return False
+    return True
+
+def prompt_for_space():
+    space = None
+    
+    try:
+        space = int(raw_input("Give me a space number from 0 to 8 (and then hit enter): "))
+    except ValueError:
+        print "That's not a number."
+        return prompt_for_space()
+
+    if not validate_move(space):
+        return prompt_for_space()
+    
+    return space
+
+
+def play(mark='X'):
+    print "\nThe board looks like this:"
+    print_board()
+    print
+
+    print "It's %s's turn - where would you like to play, %s?" % (mark, mark)
+    space = prompt_for_space()
+                
+    board[space] = mark
+
+    if win_by_diagonal(mark) or win_by_horizontal(mark) or win_by_vertical(mark):
+        print "%s wins! Good job!" % mark
+        print
+        print_board()
+    else:
+        play('O' if mark == 'X' else 'X')    
+
+play()
