1c2eebf280e514b5cb7063afac0834e457bb1850
brntbeer <brent.beer@gmail.com>
2012-10-25 15:16:25 -0700

    added turns and colors and some comments

--- shoes/connect-four.rb
+++ shoes/connect-four.rb
@@ -12,10 +12,14 @@
 # Should keep score from game to game (like galaca maybe?)
 # Have a reset/start over button.
 Shoes.app :width => 900, :height => 625 do
-  grey = rgb(166, 154, 159)
+  grey = rgb(166,154,159)
+  red = rgb(255,0,0)
+  black = rgb(0,0,0)
+
   @board = []
   @columns = [[],[],[],[],[],[],[]]
   @picked = []
+  @turn = 0
 
   6.times do |row|
     7.times do |column|
@@ -35,17 +39,25 @@ Shoes.app :width => 900, :height => 625 do
     if button == 1
       if (5..105).include?(over_x)
         if (5..105).include?(over_y)
-          # pick the bottom row for the column that was selected
-          # tricky part. we need to select the space
-          # that is within our same over_x, but at the bottom.
+          @turn+=1 # or @turn = @turn + 1 ? whichever is easier to understand
+          # Pick the bottom row for the column that was selected
+          # is the tricky part. We need to select the space
+          # that is within our same column, but at the bottom.
           # how do we do this?
           # needs to go to that columns lowest available spot
           #   save columns as arrays and push or pop like a stack.
-          color = rgb(rand(255), rand(255), rand(255))
-          #because of our column set of arrays, 
+          #color = rgb(rand(255), rand(255), rand(255))
+          #
+          #explain trinary operator? or do an if statement?
+          # check to see if after you divide the number by 2 etc,
+          # if that number eventually reaches 1, if it does, then its
+          # an odd (first turn is a base case, because its 1) and we 
+          # play red, else we play black
+          color = @turn % 2 == 1 ? red : black
+          #because of our column set of arrays,
           #we know we can take the last element there and multiply
           #it by the total number of rows to get it to the botom position.
-          #We then have to offset where that bottom position is 
+          #We then have to offset where that bottom position is
           #based on current "column" from the mouse
           space = (@columns[column].last*7)+column
           @board[space].style(:fill => color)
